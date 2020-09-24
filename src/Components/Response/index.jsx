81e374934895afd2bf5d4586ab97ddfa01a1d436
Jessica Skok <jessica.skok@gmail.com>
2020-09-23 23:43:41 -0700

    finish JSON file

--- src/Components/Response/index.jsx
+++ src/Components/Response/index.jsx
@@ -18,22 +18,36 @@ function Response({ selectedState }) {
   return (
     <div className="response">
       <div className="response-text">
-        {howMuchTime} days left!!!!!
-    </div>
+        {selectedState[0] && selectedState[0].value === 'ND' ? '' :
+          (
+            howMuchTime + ' days left'
+          )
+        }
+      </div>
       <div className="response-url">
         <div className='hurry-announcement'> {howMuchTime < 7 && `😱😱 Hurry!!!😩😩`} </div>
         <div className='registration-details'>
-          {selectedState[0] && selectedState[0].onlineAccepted ? (
-            <>
-              You can register online. Visit your state's official website {selectedState[0] && <a href={selectedState[0].url}>here.</a>}
-              {howMuchTime >= 7 && <div> You should do it now!! 👋 </div>}
-            </>
-          ) :
+          {selectedState[0] && selectedState[0].value === 'ND' ? " You don't have to register to vote. If you live in North Dakota, you're all set. Nice!" :
             (
               <>
-                You <strong>can't</strong> register online!!! Go <a href={selectedState[0] && selectedState[0].url}>here </a> for your registration form, then mail it in or drop it off before the due date! Hurry the f up!!!
-          </>
+                {selectedState[0] && selectedState[0].onlineAccepted ? (
+                  <>
+                    You can register online. Visit your state's official website {selectedState[0] && <a href={selectedState[0].url}>here.</a>}
+                    {howMuchTime >= 7 && selectedState[0] && !selectedState[0].lastMinuteAccepted && <div> You should do it now!! 👋 </div>}
+                    <div className='second-message'> {selectedState[0].lastMinuteAccepted && <div> <strong>You</strong> can even register in person on election day. Lucky you! But don't risk waiting in line or any other shenanigans, just do it now! 🇺🇸 </div>}</div>
+                  </>
+                ) :
+                  (
+                    <>
+                      You <strong>can't</strong> register online 😩 Go <a href={selectedState[0] && selectedState[0].url}>here </a> for your registration info, you can register in person or possibly mail or drop off your form before the due date. Hurry!!
+                <div className='second-message'>{selectedState[0] && selectedState[0].lastMinuteAccepted && <div> You can also register in person on election day. But don't risk waiting in line or any other shenanigans, just do it now! 🇺🇸 </div>}
+                      </div>
+                    </>
+                  )
+                }
+              </>
             )}
+
         </div>
       </div>
     </div>
