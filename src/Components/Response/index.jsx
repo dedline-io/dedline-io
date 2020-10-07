81d2b6f0dd63bd1d0c71fa1dd9df9fd0c5ba95a5
Jessica Skok <jessica.skok@livongo.com>
2020-10-06 19:27:58 -0700

    fix time stuff

--- src/Components/Response/index.jsx
+++ src/Components/Response/index.jsx
@@ -6,13 +6,19 @@ import './Response.css';
 
 function Response({ selectedState }) {
   let howMuchTime;
+  let howMuchTimeSmaller;
   const currentSelectedState = selectedState[0];
   const currentDate = moment();
 
   if (selectedState.length > 0 && currentSelectedState.deadline) {
     const momentDeadlineDate = moment(`${currentSelectedState.deadline}T235959`)
     howMuchTime = momentDeadlineDate.diff(currentDate, 'days');
+    howMuchTimeSmaller = momentDeadlineDate.diff(currentDate);
+
+    console.log(howMuchTimeSmaller);
   }
+  console.log(howMuchTime);
+  console.log(currentDate);
 
   return (
     <div className="response">
@@ -33,10 +39,10 @@ function Response({ selectedState }) {
           {currentSelectedState && currentSelectedState.value === 'ND' ? " You don't have to register to vote. If you live in North Dakota, you're all set. Nice!" :
             (
               <>
-                {howMuchTime < 0 && currentSelectedState && currentSelectedState.lasminuteAccepted && "The deadline's passed! But you can still register in person on your voting day. Lucky procrastinator! Find details at your local polling place."}
-                {howMuchTime < 0 && currentSelectedState && !currentSelectedState.lasminuteAccepted && "The deadline's passed! Hope you registered. 😢"}
-                <div className='last-day-announcement'>{howMuchTime === 0 && 'Today is the last day you can register!'}</div>
-                {howMuchTime >= 0 && currentSelectedState && currentSelectedState.onlineAccepted ? (
+                {howMuchTimeSmaller < 0 && currentSelectedState && currentSelectedState.lasminuteAccepted && "The deadline's passed! But you can still register in person on your voting day. Lucky procrastinator! Find details at your local polling place."}
+                {howMuchTimeSmaller < 0 && currentSelectedState && !currentSelectedState.lasminuteAccepted && "The deadline's passed! Hope you registered. 😢"}
+                <div className='last-day-announcement'>{howMuchTimeSmaller > 0 && howMuchTime === 0 && 'Today is the last day you can register!'}</div>
+                {howMuchTimeSmaller > 0 && howMuchTime >= 0 && currentSelectedState && currentSelectedState.onlineAccepted ? (
                   <>
                     You can register online. Visit your state's official website {currentSelectedState && <a href={currentSelectedState.url}>here.</a>}
                     {howMuchTime >= 7 && currentSelectedState && !currentSelectedState.lastMinuteAccepted && <div> You should do it now!! </div>}
@@ -46,7 +52,7 @@ function Response({ selectedState }) {
                 ) :
                   (
                     <>
-                      {howMuchTime >= 0 &&
+                      {howMuchTimeSmaller > 0 && howMuchTime >= 0 &&
                         <div>You <strong>can't</strong> register online <span role='img' aria-label='weary-guy-emoji'>😩</span> Go <a href={currentSelectedState && currentSelectedState.url}>here </a> for your registration info, you can register in person or possibly mail or drop off your form before the due date. Hurry!!
                 <div className='second-message'>{currentSelectedState && currentSelectedState.lastMinuteAccepted && <div> You can also register in person on election day. But don't risk waiting in line or any other shenanigans, just do it now if you can! <span role='img' aria-label='us-flag-emoji'>🇺🇸</span> </div>}
                           </div> </div>}
